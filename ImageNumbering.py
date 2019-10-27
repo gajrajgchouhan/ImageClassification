947d593813cabed4d10dc16f756a821df62347fc
gajrajgchouhan <54475046+gajrajgchouhan@users.noreply.github.com>
2019-10-27 23:38:47 +0530

    Update and rename untitled0.py to ImageNumbering.py

--- ImageNumbering.py
+++ ImageNumbering.py
@@ -1,8 +1,8 @@
-
+# Image numbering done sequentially
 import os
 import glob
 
 os.chdir(r"/home/gajraj/Downloads/MLChallenge/horses")
 for index, oldfile in enumerate(glob.glob("*.png"), start=1):
-    newfile = 'human-{:3}.jpg'.format(index)
+    newfile = 'horse-{:3}.jpg'.format(index)
     os.rename (oldfile,newfile)
